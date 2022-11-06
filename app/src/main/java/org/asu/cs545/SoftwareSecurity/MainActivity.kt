1e65b29df391bb4f69bcaf03e10cb359f194921e
Daljit Singh Bhalla <daljitsingh1995@gmail.com>
2022-11-05 19:51:36 -0700

    Initial commit

---
+++ app/src/main/java/org/asu/cs545/SoftwareSecurity/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.asu.cs545.SoftwareSecurity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
